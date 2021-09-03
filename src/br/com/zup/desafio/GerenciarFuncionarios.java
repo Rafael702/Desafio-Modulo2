8431bae4d0b25d78b759746e334aeabf1aee4102
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-03 13:53:07 -0300

    Entrega Média - Concluída.

--- src/br/com/zup/desafio/GerenciarFuncionarios.java
+++ src/br/com/zup/desafio/GerenciarFuncionarios.java
@@ -28,30 +28,53 @@ public class GerenciarFuncionarios {
         String telefoneFuncionario;
         String emailFuncionario;
         String cpfFuncionario;
+        boolean chave = true;
+        int opcao;
 
 
         System.out.println("Olá! Seja Bem vindo ao Programa Gerenciador de Funcionários!!!");
-        //Adicionando Funcionários
-        System.out.println("Informe o nome do funcionário: ");
-        nomeFuncionario = entrada.nextLine();
 
-        System.out.println("Informe o CPF do funcionário: ");
-        cpfFuncionario = entrada.nextLine();
 
-        System.out.println("Informe o número de telefone do funcionário para contato: ");
-        telefoneFuncionario = entrada.nextLine();
+        do {
+            System.out.println("Digite 1: Para adicionar funcionários na lista de cadastrados.");
+            System.out.println("Digite 2: Para exibir a lista de todos os funcionários cadastrados.");
+            System.out.println("Digite 3: Para Encerrar o programa.");
+            System.out.println("Escolha uma opção: ");
+            opcao = entrada.nextInt();
+            entrada.nextLine();
 
-        System.out.println("Informe o email do funcionário para contato: ");
-        emailFuncionario = entrada.nextLine();
+            if (opcao == 1) {
+                //Adicionando Funcionários
+                System.out.println("Informe o nome do funcionário: ");
+                nomeFuncionario = entrada.nextLine();
 
-        funcionarios.put("CPF do Funcionário: " + cpfFuncionario,
-                "| Nome do funcionário: " + nomeFuncionario + "| Telefone do Funcionário: " + telefoneFuncionario
-                        + "| Email do Funcionário: " + emailFuncionario);
+                System.out.println("Informe o CPF do funcionário: ");
+                cpfFuncionario = entrada.nextLine();
 
-        //Apresentando Funcionarios Cadastrados
-        System.out.println("Lista de Funcionários: ");
-        for (String dadosFuncionario : funcionarios.keySet()) {
-            System.out.println(dadosFuncionario + funcionarios.get(dadosFuncionario));
-        }
+                System.out.println("Informe o número de telefone do funcionário para contato: ");
+                telefoneFuncionario = entrada.nextLine();
+
+                System.out.println("Informe o email do funcionário para contato: ");
+                emailFuncionario = entrada.nextLine();
+
+                funcionarios.put("CPF do Funcionário: " + cpfFuncionario,
+                        "| Nome do funcionário: " + nomeFuncionario + "| Telefone do Funcionário: " + telefoneFuncionario
+                                + "| Email do Funcionário: " + emailFuncionario);
+
+            } else if (opcao == 2) {
+                //Apresentando Funcionarios Cadastrados
+                System.out.println("Lista de Funcionários: ");
+                System.out.println("----------------------------------------------------------");
+                for (String dadosFuncionario : funcionarios.keySet()) {
+                    System.out.println(dadosFuncionario + funcionarios.get(dadosFuncionario));
+                }
+                System.out.println("----------------------------------------------------------");
+
+            } else if (opcao == 3) {
+                chave = false;
+            }
+
+        } while (chave == true);
+        System.out.println("Fim do Programa.");
     }
 }
